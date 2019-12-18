c33575f688ca85334ad36c74b260ad665b72922f
Paulo Antonio <paulo.antonio@fornecedores.sicoob.com.br>
2019-12-18 11:41:20 -0300

    iniciando projeto

---
+++ src/main/java/com/pauloneto/batchapplication/producer/CDIProducer.java
@@ -0,0 +1,38 @@
+package com.pauloneto.batchapplication.producer;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.logging.Logger;
+
+/**
+ * @author paulo.antonio@fornecedores.sicoob.com.br
+ * @version 0.1 - 29 de out de 2019
+ */
+public class CDIProducer {
+
+	private static final String ACTIVEMQ_BROKER_URL = "tcp://localhost:61616?jms.rmIdFromConnectionId=true";
+
+	@PersistenceContext(unitName = "paulonetoPU")
+	private EntityManager emPauloNetoPU;
+
+	@Produces 
+	public EntityManager createEntityManager() {
+		return emPauloNetoPU;
+	}
+
+	@Produces
+	public ActiveMQConnectionFactory createMQConnectionFactory(){
+		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(ACTIVEMQ_BROKER_URL);
+		factory.setTrustAllPackages(true);
+		return factory;
+	}
+
+	@Produces
+	public Logger createLogger(InjectionPoint injectionPoint) {
+		return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+	}
+}
---
+++ src/main/java/com/pauloneto/batchapplication/repository/PerfilRepository.java
@@ -0,0 +1,14 @@
+package com.pauloneto.batchapplication.repository;
+
+import com.pauloneto.batchapplication.models.Perfil;
+import org.apache.deltaspike.data.api.EntityRepository;
+import org.apache.deltaspike.data.api.Repository;
+
+/**
+* @author paulo.antonio@fornecedores.sicoob.com.br 
+* @version 0.1 - 29 de out de 2019
+*/
+@Repository
+public interface PerfilRepository extends EntityRepository<Perfil, Long> {
+
+}
